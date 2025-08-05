8eb4e8ac8f67bc2833b6736ec1f6b99a5ce93403
tacona1016 <tacona1016@gmail.com>
2025-08-05 21:47:55 +0900

    .

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
 import streamlit as st
-st.write("test")
-st.write("test2")
+import pandas as pd
+
+st.set_page_config(
+    initial_sidebar_state="collapsed",
+    layout = "wide",
+    page_icon = "🍕",
+    page_title = "tacona1016의 개인 대시보드"
+)
+
+with st.sidebar:
+    st.write("Side bar test")
+    st.write("tacona1016의 개인 차트")
+
