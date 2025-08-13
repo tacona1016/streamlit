c1d0b1cb9f8eae562dc8b3886491c6f669d41817
tacona1016 <tacona1016@gmail.com>
2025-08-13 22:06:16 +0900

    .

--- test.py
+++ test.py
@@ -1,5 +1,29 @@
 import streamlit as st
 import pandas as pd
+import psycopg2
+import yfinance as yf
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
+import warnings
+
+# 모든 경고 무시
+warnings.filterwarnings("ignore")
+load_dotenv()
+
+host = os.environ["SUPABASE_HOST"]
+port = os.environ.get("SUPABASE_PORT", 5432)
+db   = os.environ["SUPABASE_DB"]
+user = os.environ["SUPABASE_USER"]
+pw   = os.environ["SUPABASE_PASSWORD"]
+
+url = f"postgresql+psycopg2://{user}:{pw}@{host}:{port}/{db}?sslmode=require"
+engine = create_engine(url, connect_args={"sslmode":"require"}, pool_pre_ping=True)
+
+# 연결 테스트
+with engine.begin() as conn:
+    df_test = pd.read_sql("SELECT * FROM stock_data", engine)
+tickers = list(df_test.ticker.unique())
 
 st.set_page_config(
     initial_sidebar_state="collapsed",
@@ -12,3 +36,13 @@ with st.sidebar:
     st.write("Side bar test")
     st.write("tacona1016의 개인 차트")
 
+
+st.write("test삼아 만든 차트입니다")
+
+col1, col2, col3 = st.columns([2,1,1])
+ticker = col1.selectbox('Ticker', options=tickers or ['AAPL'])
+start = col2.date_input('Start', value=pd.to_datetime('2022-01-01'))
+end = col3.date_input('End', value=pd.to_datetime('today'))
+
+df = df_test[df_test.ticker==ticker]
+st.line_chart(df.set_index('date')['close'])
